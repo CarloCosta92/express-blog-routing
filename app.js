554cc11a3235bacd9aebde1c94a64e4a5778fbce
Carlo Costa <carlo.costa92@gmail.com>
2025-03-27 14:18:09 +0100

    feat: tested call in postman

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const port = 4000;
 // importo posts.js
 const posts= require("./posts.js");
 
+// importo post router
+const postsRouter = require("./routers/posts.js")
+
 // cartella per i file pubblici
 app.use(express.static("public"));
 
@@ -12,6 +15,10 @@ app.use(express.static("public"));
 app.get("/", (req,res) => {
     res.send("Server dei post")
 });
+
+
+//rotta post router
+app.use('/posts', postsRouter);
 
 //definizione rotta /bacheca
 app.get("/bacheca" , (req,res) => {
