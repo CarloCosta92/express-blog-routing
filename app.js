4ef8491576abd7bb5f3dda8881d4d20068e7888a
Carlo Costa <carlo.costa92@gmail.com>
2025-03-27 13:09:28 +0100

    feat: new route defined

--- app.js
+++ app.js
@@ -2,9 +2,23 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+// importo posts.js
+const posts= require("./posts.js");
+
+// cartella per i file pubblici
+app.use(express.static("public"));
+
+
 app.get("/", (req,res) => {
-    res.send("Ciao Carlo")
+    res.send("Server dei post")
 });
+
+//definizione rotta /bacheca
+app.get("/bacheca" , (req,res) => {
+    res.json(posts);
+});
+
+
 
 app.listen(port, () => {
     console.log(`app in ascolto sulla porta `+ port)
